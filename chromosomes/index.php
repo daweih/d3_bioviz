f1b2ccc344d9e86f21500e5ab68c0af90507639a
dawei huang <daweih@me.com>
2015-07-17 11:35:59 +0800

    add font.

--- chromosomes/index.php
+++ chromosomes/index.php
@@ -3,6 +3,48 @@
     <head>
         <meta charset="utf-8" />
         <title>svg drawer</title>
+            <style>
+				body {
+				}
+				@font-face {
+					font-family: 'Helvetica Neue UltraLight';
+					src: url('../font/helvetica_neue_ultralight.ttf');
+				}
+				@font-face {
+					font-family: 'Helvetica Neue Light';
+					src: url('../font/helvetica_neue_light.ttf');
+				}
+				@font-face {
+					font-family: 'Helvetica';
+					src: url('../font/helvetica.ttf');
+				}
+				@font-face {
+					font-family: 'Helvetica Neue Bold';
+					src: url('../font/helvetica_neue_bold.ttf');
+				}
+				@font-face {
+					font-family: 'Corbel Bold';
+					src: url('../font/corbel_bold.ttf');
+				}
+				@font-face {
+					font-family: 'Calisto MT';
+					src: url('../font/calisto_mt.ttf');
+				}
+				@font-face {
+					font-family: 'Candara Bold';
+					src: url('../font/candara_bold.ttf');
+				}
+				@font-face {
+					font-family: 'Candara';
+					src: url('../font/candara.ttf');
+				}
+			div.canvas {
+/*				-webkit-filter: blur(6px);*/
+				
+			}
+			</style>
+   
+        
     </head>
     
     <body>
